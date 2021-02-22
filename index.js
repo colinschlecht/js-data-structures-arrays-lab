2a1f3026a75691b0792e5f725c014474f3f236a5
Colin Schlecht <schlechtcolin@yahoo.com>
2021-02-21 23:42:13 -0800

    commit tree 77ef24bf767d694a86fe024efe2e15210cb5e0e4

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+let drivers = ["Milo", "Otis", "Garfield"];
+
+ 
+function destructivelyAppendDriver(name){
+    drivers.push(name)  
+} 
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+function destructivelyRemoveLastDriver(name){
+    drivers.pop(name)
+}
+function destructivelyRemoveFirstDriver(name){
+    drivers.shift(name)
+}
+function appendDriver(name){
+    let newDrivers = [...drivers.slice(), name]
+    return newDrivers
+}
+function prependDriver(name){
+    let newDrivers = [name, ...drivers.slice()]
+    return newDrivers
+}
+function removeLastDriver(){
+    let newDrivers = [...drivers.slice(0, drivers.length -1)]
+    return newDrivers
+    
+}
+function removeFirstDriver(name){
+    let newDrivers = [...drivers.slice(1)]
+    return newDrivers
+}
